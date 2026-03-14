9f0baafa4cc604ed0b171da1f17aea1d56571756
SorThanaroth <sorthanaroth018@gmail.com>
2026-03-14 20:26:13 +0700

    fix: 🎈 mongodb v2

--- configs/db.js
+++ configs/db.js
@@ -3,19 +3,19 @@ const mongoose = require('mongoose');
 module.exports = async () => {
   try {
     // mongodb://localhost:27017/e_commerce
-    // await mongoose.connect('mongodb+srv://sorthanaroth018_db_user:j6LnYUv5Umr1gB1e@cluster0.mbol6gv.mongodb.net/?appName=Cluster0', {
-    //   autoIndex: true,
-    //   serverSelectionTimeoutMS: 30000 // default 30 seconds
-    // });
+    await mongoose.connect('mongodb+srv://sorthanaroth018_db_user:j6LnYUv5Umr1gB1e@cluster0.mbol6gv.mongodb.net/?appName=Cluster0', {
+      autoIndex: true,
+      serverSelectionTimeoutMS: 30000 // default 30 seconds
+    });
     console.log("URI Check:", process.env.MONGODB_URI); // <--- Add this
     
     if (!process.env.MONGODB_URI) {
       throw new Error("MONGODB_URI is not defined in environment variables");
     }
-     await mongoose.connect(process.env.MONGODB_URI, {
-      autoIndex: true,
-      serverSelectionTimeoutMS: 30000 // default 30 seconds
-    });
+    //  await mongoose.connect(process.env.MONGODB_URI, {
+    //   autoIndex: true,
+    //   serverSelectionTimeoutMS: 30000 // default 30 seconds
+    // });
     console.log("MongoDB connected~");
   } catch (err) {
     console.log("Mongoose: ",err);
